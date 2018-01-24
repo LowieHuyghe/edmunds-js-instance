94bc32a6340719c8f14cb96205ed1348d78ae988
Lowie Huyghe <iam@lowiehuyghe.com>
2018-01-24 22:24:35 +0100

    Added middleware to get started developing.

--- src/bootstrap/app.ts
+++ src/bootstrap/app.ts
@@ -4,17 +4,25 @@ import {
 } from 'edmunds'
 import { route } from '../app/http/routes'
 import { Handler } from '../app/errors/handler'
+import * as errorHandler from 'errorhandler'
+import * as bodyParser from 'body-parser'
 
 export function bootstrap (): Edmunds {
   const edmunds = new Edmunds()
 
-  // Logging Service Provider
+  // Service Providers
   edmunds.register(LoggingServiceProvider)
+
+  // Body parser
+  edmunds.app.use(bodyParser.json())
 
   // Add the routes
   route(edmunds)
 
-  // Add error handler
+  // Error handling
+  if (edmunds.isDevelopment()) {
+    edmunds.app.use(errorHandler())
+  }
   edmunds.app.use(Handler.func())
 
   return edmunds
