c0dc52021faf29a2166438e898a3ce70769d33b7
Lowie Huyghe <iam@lowiehuyghe.com>
2019-01-17 00:25:33 +0100

    Fix for ormconfig.js

--- ormconfig.js
+++ ormconfig.js
@@ -2,7 +2,7 @@ const config = require('config')
 
 let instances = []
 if (config.has('database.instance')) {
-  instances = [config.has('database.instance')]
+  instances = [config.get('database.instance')]
 } else if (config.has('database.instances')) {
   instances = config.get('database.instances')
 }
